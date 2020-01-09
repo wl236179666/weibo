b872b1c208b5bb8b68141d6f676ed73eaf5931d0
wangliang <236179666@qq.com>
2020-01-09 11:06:29 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,41 @@ class User extends Authenticatable
     {
         return $this -> statuses() -> orderBy('created_at','desc');
     }
+
+    //粉丝列表
+    public function followers()
+    {
+        return $this -> belongsToMany(User::Class,'followers','user_id','follower_id');
+    }
+
+    //关注列表
+    public function followings()
+    {
+        return $this -> belongsToMany(User::Class,'followers','follower_id','user_id');
+    }
+
+    //关注
+    public function follow($user_ids)
+    {
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this -> followings() -> sync($user_ids,false);
+    }
+
+
+    //取消关注
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this -> followings() -> detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this -> followings() -> contains($user_id);
+    }
 }
