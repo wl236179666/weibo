0b7111afdd23b499bb61a8611a343240202910a9
wangliang <236179666@qq.com>
2020-01-09 15:13:01 +0000

    关注按钮

--- app/Models/User.php
+++ app/Models/User.php
@@ -97,6 +97,6 @@ class User extends Authenticatable
 
     public function isFollowing($user_id)
     {
-        return $this -> followings() -> contains($user_id);
+        return $this->followings -> contains($user_id);
     }
 }
